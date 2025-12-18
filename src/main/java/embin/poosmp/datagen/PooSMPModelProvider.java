5fffe614c673c1030617eb52c42b2db849c8157a
Embin <113154360+NotEmbin@users.noreply.github.com>
2025-12-18 12:41:00 -0800

    Update PooSMPModelProvider.java

--- src/main/java/embin/poosmp/datagen/PooSMPModelProvider.java
+++ src/main/java/embin/poosmp/datagen/PooSMPModelProvider.java
@@ -1,12 +1,10 @@
 package embin.poosmp.datagen;
 
 import embin.poosmp.PooSMPMod;
-import embin.poosmp.items.PooSMPItems;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
-import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.BlockItem;
@@ -18,6 +16,7 @@ public class PooSMPModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
+        // why do i HAVE to put item model generation in the BLOCK STATE MODEL GEN FUNCTION
         BuiltInRegistries.ITEM.stream().filter(item -> item.getCreatorNamespace(item.getDefaultInstance()).equals(PooSMPMod.MOD_ID)).forEach(item -> {
             Identifier itemId = BuiltInRegistries.ITEM.getKey(item);
             Identifier modelId = item instanceof BlockItem ? itemId.withPrefix("block/") : itemId.withPrefix("item/");
