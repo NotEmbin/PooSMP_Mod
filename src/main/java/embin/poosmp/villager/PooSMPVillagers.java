6e379d9f1d6eff7397cfac836f3aa5ad59c8aae6
Embin <113154360+NotEmbin@users.noreply.github.com>
2025-12-17 20:07:54 -0800

    Update PooSMPVillagers.java

--- src/main/java/embin/poosmp/villager/PooSMPVillagers.java
+++ src/main/java/embin/poosmp/villager/PooSMPVillagers.java
@@ -5,6 +5,7 @@ import embin.poosmp.PooSMPMod;
 import embin.poosmp.util.Id;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
@@ -18,7 +19,7 @@ public class PooSMPVillagers {
 
     private static VillagerProfession register(String name, ResourceKey<PoiType> type, SoundEvent sound_event) {
         return Registry.register(BuiltInRegistries.VILLAGER_PROFESSION, Id.of(name),
-                new VillagerProfession(name, entry -> entry.is(type),
+                new VillagerProfession(Component.literal(name), entry -> entry.is(type),
                         entry -> entry.is(type), ImmutableSet.of(),
                         ImmutableSet.of(), sound_event));
     }
